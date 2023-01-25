3f66f71c320849322a91efe80a837c67a7dcaa87
Htoo Maung Thait <htoomaungthait@gmail.com>
2023-01-25 23:26:42 +0630

    mongoose db crud finished

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,4 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Body,
+  Param,
+  Delete,
+  Query,
+} from '@nestjs/common';
+import { CarDto } from './car.dto';
 import { CarService } from './car.service';
 
 @Controller('car')
@@ -6,7 +16,33 @@ export class CarController {
   constructor(private carService: CarService) {}
 
   @Get()
-  async getCars() {
+  public async getCars() {
     return this.carService.getCars();
+  }
+
+  @Post()
+  public async postCar(@Body() car: CarDto) {
+    return this.carService.postCar(car);
+  }
+
+  @Get(':id')
+  public async getCarById(@Param('id') id: number) {
+    console.log('The requested id : ', id);
+    return this.carService.getCarById(id);
+  }
+
+  @Delete(':id')
+  public async deleteCarById(@Param('id') id: number) {
+    return this.carService.deleteCarById(id);
+  }
+
+  @Put(':id')
+  public putCarById(@Param('id') id: number, @Query() query) {
+    console.log('The id is : ', id);
+    console.log('This query is : ', query);
+    const propertyName = query.property_name;
+    const propertyValue = query.property_value;
+
+    return this.carService.putCarById(id, propertyName, propertyValue);
   }
 }
--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,11 +1,58 @@
-import { Injectable } from '@nestjs/common';
-import { CARS } from './cars.mock';
+import { HttpException, Injectable } from '@nestjs/common';
+import { CarDto } from './car.dto';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { ICar } from './interfaces/Car.interfaces';
+
+const carProjection = {
+  _v: false,
+  _id: false,
+};
 
 @Injectable()
 export class CarService {
-  private cars = CARS;
+  constructor(@InjectModel('Car') private readonly carModel: Model<ICar>) {}
 
-  public async getCars() {
-    return this.cars;
+  public async getCars(): Promise<CarDto[]> {
+    const cars = await this.carModel.find({}, carProjection).exec();
+    if (!cars || !cars[0]) {
+      throw new HttpException('Not Found', 404);
+    }
+    return cars;
+  }
+
+  public async postCar(nawCar: CarDto) {
+    const car = await new this.carModel(nawCar);
+    return car.save();
+  }
+
+  public async getCarById(id): Promise<CarDto> {
+    const car = await this.carModel.findOne({ id }, carProjection).exec();
+    if (!car) {
+      throw new HttpException('Not Found', 404);
+    }
+    return car;
+  }
+
+  public async deleteCarById(id: number): Promise<any> {
+    const car = await this.carModel.deleteOne({ id }).exec();
+    if (car.deletedCount === 0) {
+      throw new HttpException('Not Found', 404);
+    }
+    return car;
+  }
+
+  public async putCarById(
+    id: number,
+    propertyName: string,
+    propertValue: string,
+  ) {
+    const car = await this.carModel
+      .findOneAndUpdate({ id }, { [propertyName]: propertValue })
+      .exec();
+    if (!car) {
+      throw new HttpException('Not Found', 404);
+    }
+    return car;
   }
 }
